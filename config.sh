909d2e22abd9db3062281e5daf30cb6bf2c15eed
chomusuke-mk <231588837+chomusuke-mk@users.noreply.github.com>
2026-07-12 21:29:46 -0500

    0.15.1

--- config.sh
+++ config.sh
@@ -3,4 +3,4 @@
 # Configuración de QuickJS
 export QUICKJS_REPO="https://github.com/quickjs-ng/quickjs.git"
 export QUICKJS_VERSION="v0.15.1"
-export VIDRA_QUICKJS_VERSION="0.8.0"
+export VIDRA_QUICKJS_VERSION="0.15.1"
