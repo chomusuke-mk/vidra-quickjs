376bc78441c98369f59852b1d50ac114db77ffe2
chomusuke-mk <231588837+chomusuke-mk@users.noreply.github.com>
2026-07-13 14:39:15 -0500

    actualizar la versión de VIDRA_QUICKJS a 0.15.1.1

--- config.sh
+++ config.sh
@@ -3,4 +3,4 @@
 # Configuración de QuickJS
 export QUICKJS_REPO="quickjs-ng/quickjs"
 export QUICKJS_VERSION="v0.15.1"
-export VIDRA_QUICKJS_VERSION="0.15.1"
+export VIDRA_QUICKJS_VERSION="0.15.1.1"
